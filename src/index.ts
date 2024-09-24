57eba5b574bf5e1ab2f3f4c7a4266711a153aa3b
Dzikri-Ardafi <dzkri.ardfi@gmail.com>
2024-09-24 16:29:53 +0700

    initial commit for websocket

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,11 @@ import cors, { CorsOptions } from "cors";
 import dbConnectionHandler from "./utils/dbConnectionHandler";
 import createRouter from "./routes/api";
 import { CollectionInfo, Db } from "mongodb";
+import ws from "ws";
 
 const app: Express = express();
 const appPort = process.env.PORT || 3000;
-const wsPort = process.env.PORT_WS || 8080;
+const wsPort = process.env.PORT_WS || (8080 as any);
 
 /* ------------------------------- Cors config ------------------------------ */
 const corsOptions = {
@@ -20,17 +21,42 @@ const corsOptions = {
 } as CorsOptions;
 app.use(cors(corsOptions));
 
+/* ---------------------------- Websocket config ---------------------------- */
+const wss = new ws.Server({
+  port: wsPort,
+});
+
+wss.on("connection", (websocket) => {
+  console.log("A new client connected");
+
+  // --> incomming message
+  websocket.on("message", (message) => {
+    console.log("Received message:", message.toString());
+
+    // --> send message to all connected client
+    wss.clients.forEach((client) => {
+      if (client.readyState === ws.OPEN) {
+        client.send("Hello from server");
+      }
+    });
+  });
+
+  // --> client disconnected
+  websocket.on("close", () => {
+    console.log("A client disconnected");
+  });
+});
+
 /* ------------------------------ Route config ------------------------------ */
 const db = dbConnectionHandler.dbClient.db("iot") as Db;
 app.use("/api", createRouter(db));
-app.get("/", (req, res) => {
-  res.send("Welcome to my app");
-});
 
 /* --------------------------------- Run app -------------------------------- */
 app.listen(appPort, async () => {
   try {
-    await dbConnectionHandler.connectDBHandler();
+    // await dbConnectionHandler.connectDBHandler();
+    console.log(`Ws port`, wsPort);
+
     console.log(`http://localhost:${appPort}`);
   } catch (error) {
     console.log(error);
